9666aa6a30c2bfa77cb73d1862f1546b86561cfe
Juener <juener@gmail.com>
2024-04-21 07:08:12 +0100

    feat: fetch user checkin history with pagination

--- nodejs/src/repositories/in-memory/in-memory-check-ins.ts
+++ nodejs/src/repositories/in-memory/in-memory-check-ins.ts
@@ -20,6 +20,14 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
     return checkIn
   }
 
+  async findManyByUserId(userId: string, page: number) {
+    const checkIns = this.items
+      .filter((checkIn) => checkIn.user_id === userId)
+      .slice((page - 1) * 20, page * 20)
+
+    return checkIns
+  }
+
   async findByUserIdOnDate(userId: string, date: Date) {
     const startOfTheDay = dayjs(date).startOf('date')
     const endOfTheDay = dayjs(date).endOf('date')
